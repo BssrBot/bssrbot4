3635e1547db6c240f2d5ef0b1aff99c81ae8d3a2
Zachary Stone <ZacharyStone@MacBook-Pro-3.hitronhub.home>
2022-12-19 10:19:17 -0800

    added emojis to commands list to make it easier to read

--- src/bssrBotFunctions/getCommands.js
+++ src/bssrBotFunctions/getCommands.js
@@ -29,26 +29,31 @@ crack a joke --> bssr bot will tell you a joke
 !! Secret quotes exist !! --> input your friends and see if bssrbot has anything to say about them
 
 */
-
-
-//console.log(emoji.get('coffee'));
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const emoji = require('node-emoji');
 
 export function getCommands() {
 	return `COMMAND LIST\n\n` + health() + dino() + college() + fun();
 }
 
 function health() {
-	return `Health Services:\nhealth --> details of UNSW health services, dentist, optometrist, counsellor, Helpline, Lifeline, duty tutor, and security\n\n\n`;
+	const hospital = emoji.get('hospital');
+	return `Health Services ${hospital}:\nhealth --> details of UNSW health services, dentist, optometrist, counsellor, Helpline, Lifeline, duty tutor, and security\n\n\n`;
 }
 
 function dino() {
-	return `Dino:\nbreakfast --> get todays breakfast\n\nlunch --> get todays lunch\n\ndinner --> tonight ill be eating\n\ndino --> get todays menu\n\ndino times --> gives the times of dino breakfast lunch and dinner\n\ndino feedback --> leave feedback on your scrumptious meal!\n\n\n`
+	const dino = emoji.get('knife_fork_plate')
+	return `Dino ${dino}:\nbreakfast --> get todays breakfast\n\nlunch --> get todays lunch\n\ndinner --> tonight ill be eating\n\ndino --> get todays menu\n\ndino times --> gives the times of dino breakfast lunch and dinner\n\ndino feedback --> leave feedback on your scrumptious meal!\n\n\n`
 }
 
 function college() {
-	return `College:\nlaundry --> get the link to the laundry portal\n\ncollege feedback --> write feedback for events or general college life\n\nwildcat --> wildcat of the week nomination form\n\nrecap --> recappuccino\n\ncoffee night --> submit photos or quotes for coffee night\n\nwhats on? --> event scedule for the week\n\nwhats on today? --> todays events\n\nwhats on tomorrow? --> tomorrows events\n\n\n`
+	const college = emoji.get('heart') + emoji.get('blue_heart') + emoji.get('white_heart')
+	return `College ${college}:\nlaundry --> get the link to the laundry portal\n\ncollege feedback --> write feedback for events or general college life\n\nwildcat --> wildcat of the week nomination form\n\nrecap --> recappuccino\n\ncoffee night --> submit photos or quotes for coffee night\n\nwhats on? --> event scedule for the week\n\nwhats on today? --> todays events\n\nwhats on tomorrow? --> tomorrows events\n\n\n`
 }
 
 function fun() {
-	return `Fun!:\ncrack a joke --> bssr bot will tell you a joke\n\n!! Secret quotes exist !!--> input your friend's names and see if bssrbot has anything to say about them ;)\n`
+	const fun = emoji.get('tada')
+	return `Fun! ${fun}:\ncrack a joke --> bssr bot will tell you a joke\n\n!! Secret quotes exist !!--> input your friend's names and see if bssrbot has anything to say about them ;)\n`
 }
+
