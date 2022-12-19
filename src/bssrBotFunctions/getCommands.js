93d971c7cda1ec50717bed8e482f121eaceb48f5
Catoonza <lorenzocat03@gmail.com>
2022-12-19 16:29:30 +1100

    modifying messages for better formatting

--- src/bssrBotFunctions/getCommands.js
+++ src/bssrBotFunctions/getCommands.js
@@ -31,27 +31,46 @@ crack a joke --> bssr bot will tell you a joke
 */
 
 export function getCommands() {
-	return 'COMMAND LIST\n\
-	Health Services:\n\
-	health --> details of UNSW health services, dentist, optometrist, counsellor, Helpline, Lifeline, duty tutor, and security\n\n\
-	Dino:\n\
-	breakfast --> get todays breakfast\n\
-	lunch --> get todays lunch\n\
-	dinner --> tonight ill be eating\n\
-	dino --> get todays menu\n\
-	dino times --> gives the times of dino breakfast lunch and dinner\n\
-	dino feedback --> leave feedback on your scrumptious meal!\n\n\
-	College:\n\
-	laundry --> get the link to the laundry portal\n\
-	college feedback --> write feedback for events or general college life\n\
-	wildcat --> wildcat of the week nomination form\n\
-	recap --> recappuccino\n\
-	coffee night --> submit photos or quotes for coffee night\n\
-	whats on? --> event scedule for the week\n\
-	whats on today? --> todays events\n\
-	whats on tomorrow? --> tomorrows events\n\n\
-	Fun!:\n\
-	crack a joke --> bssr bot will tell you a joke\n\
-	!! Secret quotes exist !! --> input your friends and see if bssrbot has anything to say about them\n\
-	'
+	return `COMMAND LIST\n\n` + health() + dino(); + college() + fun();
+}
+
+function health() {
+	return `
+	Health Services:\n
+	<u>health</u> --> details of UNSW health services, dentist, optometrist, counsellor, Helpline, Lifeline, duty tutor, and security\n\n
+	`;
+}
+
+function dino() {
+	return `
+	Dino:\n
+	<u>breakfast</u> --> get todays breakfast\n
+	<u>lunch</u> --> get todays lunch\n
+	<u>dinner</u> --> tonight ill be eating\n
+	<u>dino</u> --> get todays menu\n
+	<u>dino times</u> --> gives the times of dino breakfast lunch and dinner\n
+	<u>dino feedback</u> --> leave feedback on your scrumptious meal!\n\n
+	`
+}
+
+function college() {
+	return `
+	College:\n
+	<u>laundry</u> --> get the link to the laundry portal\n
+	<u>college</u> feedback --> write feedback for events or general college life\n
+	<u>wildcat</u> --> wildcat of the week nomination form\n
+	<u>recap</u> --> recappuccino\n
+	<u>coffee night</u> --> submit photos or quotes for coffee night\n
+	<u>whats on?</u> --> event scedule for the week\n
+	<u>whats on today?</u> --> todays events\n
+	<u>whats on tomorrow?</u> --> tomorrows events\n\n
+	`
+}
+
+function fun() {
+	return `
+	Fun!:\n
+	<u>crack a joke</u> --> bssr bot will tell you a joke\n
+	<u>!! Secret quotes exist !!</u>--> input your friend's names and see if bssrbot has anything to say about them ;)\n\
+	`
 }
