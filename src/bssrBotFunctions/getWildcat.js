90e47126dd66f38a42e0262cfc13d0fbe58afa37
Zachary Stone <ZacharyStone@MacBook-Pro-3.hitronhub.home>
2022-12-22 10:40:31 -0800

    added a getquotes

--- src/bssrBotFunctions/getWildcat.js
+++ src/bssrBotFunctions/getWildcat.js
@@ -1,8 +1,12 @@
 const listOfQuotes = [];
 
 export function addQuote(text) {
+    let newText = text.replace("coffee night quote", "");
+    listOfQuotes.push(newText);
+}
 
-
+export function getQuotes() {
+    
 }
 
 export function getWildcat() {
