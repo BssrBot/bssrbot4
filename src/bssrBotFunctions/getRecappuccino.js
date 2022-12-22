9a79666ff0f75e0714566800b90cbf8a4de753e4
Catoonza <lorenzocat03@gmail.com>
2022-12-22 17:29:13 +1100

    recap working link provided

--- src/bssrBotFunctions/getRecappuccino.js
+++ src/bssrBotFunctions/getRecappuccino.js
@@ -9,7 +9,7 @@ export function validRecap(text) {
 }
 
 export function getRecappuccino(recapWeek) {
-	if (recapWeek === '' || recapWeek === undefined) {
+	if (recapWeek === '' || recapWeek === undefined || parseInt(recapWeek) > parseInt(latestWeek())) {
 		recapWeek = latestWeek();
 	}
 	const pathname = './src/bssrBotFunctions/RecappuccinoPDF/' + recapWeek + '.txt';
