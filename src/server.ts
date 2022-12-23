e3404f1ca4bf0c7808cef8d68e4d33c89aa20638
Zachary Stone <zacharynoahstone@gmail.com>
2022-12-23 15:39:14 -0800

    adding comments

--- src/server.ts
+++ src/server.ts
@@ -99,8 +99,6 @@ function handleMessage(senderPsid, receivedMessage) {
     // Create the payload for a basic text message, which
     // will be added to the body of your request to the Send API
 
-    // Get coffee night pics has to be in server.ts because it requires use of callSendAPI several times
-
     response = Respond(senderPsid, receivedMessage.text);
   } else if (receivedMessage.attachments) {
 
