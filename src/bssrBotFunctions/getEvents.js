591c414ded4b66bf9a4383062355edd5301c8647
Catoonza <lorenzocat03@gmail.com>
2022-12-23 14:00:34 +1100

    promise issues

--- src/bssrBotFunctions/getEvents.js
+++ src/bssrBotFunctions/getEvents.js
@@ -5,7 +5,7 @@ const ical = require('node-ical');
 const CALENDAR = 'https://calendar.google.com/calendar/ical/fed4287b9c43ffbee8f81e3d31b298f94b49389c7b82d63d849b98d8992b8a61%40group.calendar.google.com/public/basic.ics';
 
 export function getWhatsOn() {
-    const events = ical.sync.fromURL(CALENDAR);
+    const events = ical.fromURL(CALENDAR);
     let eventList = '';
     for (const event in events) {
         if (events[event].type === 'VEVENT') {
