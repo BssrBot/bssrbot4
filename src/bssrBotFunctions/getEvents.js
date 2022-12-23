00ebcc1d967ffff26eb39e5344de5f5a9b7206e4
Catoonza <lorenzocat03@gmail.com>
2022-12-23 14:51:50 +1100

    restrictions to calendar

--- src/bssrBotFunctions/getEvents.js
+++ src/bssrBotFunctions/getEvents.js
@@ -19,8 +19,7 @@ export function getWhatsOn() {
     for (const event in events) {
         if (events[event].type === 'VEVENT') {
             console.log('Start:', startOfWeek, 'End:', endOfWeek);
-            console.log(events[event].summary >= startOfWeek);
-            if (events[event].summary >= startOfWeek && events[event].summary <= endOfWeek)
+            if (new Date(events[event].start) >= startOfWeek && new Date(events[event].start) <= endOfWeek)
             eventList += events[event].summary + '\t' + events[event].start + '\n\n';
         }
     }
