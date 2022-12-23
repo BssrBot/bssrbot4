cb4553fc999dc9b525a8e07a325e68e6830da37f
Catoonza <lorenzocat03@gmail.com>
2022-12-23 12:58:16 +1100

    attempting to connect calendar to the bot

--- src/bssrBotFunctions/getEvents.js
+++ src/bssrBotFunctions/getEvents.js
@@ -4,8 +4,10 @@ const require = createRequire(import.meta.url);
 const ical = require('node-ical');
 const CALENDAR = 'https://calendar.google.com/calendar/ical/fed4287b9c43ffbee8f81e3d31b298f94b49389c7b82d63d849b98d8992b8a61%40group.calendar.google.com/public/basic.ics';
 
-export function getWhatsOn() {
-    return ical.async.fromURL(CALENDAR, function(err, data) { console.log(data); });
+export async function getWhatsOn() {
+    const events = await ical.async.fromURL(CALENDAR);
+    console.log(events);
+    return events;
 }
 
 export function getWhatsOnToday() {
