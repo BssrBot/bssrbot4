bd513f7fdf0240271052580de6ad19b657259feb
Zachary Stone <zacharynoahstone@gmail.com>
2023-06-29 15:05:37 +1000

    fixed feedback form

--- src/bssrBotFunctions/getFeedback.js
+++ src/bssrBotFunctions/getFeedback.js
@@ -7,7 +7,7 @@ export function getFeedback() {
 			"buttons":[
 				{
 					"type":"web_url",
-					"url":'https://forms.gle/xdgJ7WRc6NVYY5No6',
+					"url":'https://forms.gle/XrDnFPAp55MKcRnL7',
 					"title":"Feedback",
 					"webview_height_ratio": "full"
 				}
